3689de1dd38d586ac8934f8a72b8cb8fdff71f94
thanujadev26 <thanujapriyadarshane26@gmail.com>
2025-03-26 22:13:37 +0530

    Year dynamic

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function Footer() {
+  let date = new Date().getFullYear() 
   return (
     <>
       <footer className="py-8 text-center relative text-white">
         <div className="absolute inset-0 rounded-3xl text-white"></div>
         <p className="relative text-white text-md sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
-          &copy; 2024 Thanuja Priyadarshane. All Rights Reserved.
+          &copy; <span>
+            {
+              date
+            }
+          </span> Thanuja Priyadarshane. All Rights Reserved.
         </p>
       </footer>
     </>
